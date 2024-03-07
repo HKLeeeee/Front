8aee7cd9f04b720f956996953a126f7a22cc1e8f
HKLeeeee <dlapdlf1739@gmail.com>
2024-03-07 21:24:25 +0900

    feat: 이미지 리스트 상태 업데이트하여 컴포넌트 추가하기

--- image-resize/src/components/ImageContainer.tsx
+++ image-resize/src/components/ImageContainer.tsx
@@ -1,21 +1,24 @@
 import ImageEach from "./ImageEach.tsx";
 import ImageUpload from "./ImageUploade.tsx";
+import {useState} from "react";
 
 function ImageContainer() {
-    const image_src = [
+    const prev = [
         "https://flexible.img.hani.co.kr/flexible/normal/960/960/imgdb/resize/2019/0121/00501111_20190121.JPG",
         "https://cdn.imweb.me/upload/S201910012ff964777e0e3/62f9a36ea3cea.jpg",
         "https://img.freepik.com/free-photo/cute-puppy-sitting-in-grass-enjoying-nature-playful-beauty-generated-by-artificial-intelligence_188544-84973.jpg",
         "https://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg",
         "https://img.freepik.com/free-photo/cute-puppy-sitting-in-grass-enjoying-nature-playful-beauty-generated-by-artificial-intelligence_188544-84973.jpg",
     ]
+    const [image_src, setImageSrc] = useState<string[]>(prev);
 
     const image_components = image_src.map(s => {
         return <ImageEach src={s}/>
     })
+
     return (
         <div className="m-5 pt-3 grid grid-cols-4 gap-4">
-            <ImageUpload />
+            <ImageUpload setImageSrc={setImageSrc} />
             {image_components}
         </div>
     )
--- image-resize/src/components/ImageUploade.tsx
+++ image-resize/src/components/ImageUploade.tsx
@@ -1,6 +1,44 @@
-import {handelFileChange} from "../apis/handelFileChange.ts";
+import React from "react";
+import AWS from "aws-sdk";
 
-function ImageUpload() {
+type uploadProps = {
+    setImageSrc : React.Dispatch<React.SetStateAction<string[]>>
+}
+function ImageUpload(props: uploadProps) {
+
+    const handelFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            const file = event.target.files[0]
+            AWS.config.update({
+                accessKeyId: import.meta.env.VITE_ACCESSKEY,
+                secretAccessKey: import.meta.env.VITE_SECRETKEY,
+                region: 'ap-northeast-2'
+            });
+
+            const s3 = new AWS.S3();
+            const fileName = file.name;
+            const fileKey = 'images/' + fileName; // S3에 저장될 경로
+
+            const params = {
+                Bucket: import.meta.env.VITE_BUCKET,
+                Key: fileKey,
+                Body: file,
+                ACL: 'public-read',
+                ContentType: file.type
+            };
+
+            s3.upload(params, function (err: Error, data: any) {
+                if (err) {
+                    console.error('S3 업로드 오류:', err);
+                    return;
+                }
+                console.log('S3에 이미지 업로드 완료:', data.Location);
+                props.setImageSrc(prev => [...prev, data.Location]);
+            });
+        }
+    }
+
+
     return (
         <div className="h-full w-full flex flex-col gap-2 justify-center items-center">
             <div>
