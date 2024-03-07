6cc9c1250841edad593cb5caa6900bee4df89436
HKLeeeee <dlapdlf1739@gmail.com>
2024-03-07 15:13:55 +0900

    feat: init project

---
+++ image-resize/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    console.log(import.meta.env.VITE_SOME_KEY)
+  return (
+    <div>
+        <h1>
+            Hello World!
+        </h1>
+    </div>
+  )
+}
+
+export default App
