0f30fda938ccd8cf69650b0044945e5947ec9459
HKLeeeee <dlapdlf1739@gmail.com>
2024-03-07 16:24:28 +0900

    feat: header 생성

---
+++ image-resize/src/components/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="flex justify-between">
+          <div>01</div>
+          <div>02</div>
+          <div>03</div>
+      </div>
+  )
+}
+
+export default App
